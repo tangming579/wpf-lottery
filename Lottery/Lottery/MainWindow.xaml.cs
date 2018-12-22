6bdaca445079b88f374612aec5bc8ce63256d5eb
Tangming <infly123@sina.com>
2018-12-22 22:30:36 +0800

    update

--- Lottery/Lottery/MainWindow.xaml.cs
+++ Lottery/Lottery/MainWindow.xaml.cs
@@ -20,9 +20,66 @@ namespace Lottery
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _isEntrieView = false; //全屏状态
+
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            EntireView();
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Escape)
+                {
+                    EntireView();
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        private void EntireView()
+        {
+            if (!_isEntrieView)
+            {
+                //设置全屏
+                this.WindowState = WindowState.Normal;
+                this.WindowStyle = WindowStyle.None;
+                this.ResizeMode = ResizeMode.NoResize;
+                //this.Topmost = true;
+                //this.IsTopmost = true;
+                //this.CanClose = false;
+                //this.CanMove = false;
+
+                this.Left = 0;
+                this.Top = 0;
+                this.Width = SystemParameters.PrimaryScreenWidth;
+                this.Height = SystemParameters.PrimaryScreenHeight;
+
+                _isEntrieView = true;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+                this.ResizeMode = ResizeMode.CanResize;
+                //this.CanClose = true;
+                //this.CanMove = true;
+
+                this.Left = 100;
+                this.Top = 100;
+                this.Width = 800;
+                this.Height = 600;
+
+                _isEntrieView = false;
+            }
         }
     }
 }
